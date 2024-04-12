23bf9b444fbabe3aa94d6ca083e083089d2cc949
azizkhonteacher <azizkhonteacher@gmail.com>
2024-04-12 14:21:52 +0500

    js ishladi

--- javascript/app.js
+++ javascript/app.js
@@ -8,23 +8,19 @@ const shapeValue = document.getElementById("shape-value");
 
 shapeValue.textContent = shape1Qiymati;
 
-function shapeColor() {
-  const shape1_style = `conic-gradient(
-        #134A45 0%,
-        #134A45 ${shape1Qiymati}%,
-        #CBD0D5 ${shape1Qiymati}%,
-        #CBD0D5 100% 
-        );`;
+const shape1_style = `conic-gradient(
+  #134A45 0%,
+  #134A45 ${shape1Qiymati}%,
+  #CBD0D5 ${shape1Qiymati}%,
+  #CBD0D5 100% 
+  );`;
 
-  shape_1.style.background = shape1_style;
-
-  const shape2_style = `conic-gradient(
+const shape2_style = `conic-gradient(
     #508883 0%,
     #508883 ${shape2Qiymati}%,
     #96c9e9 ${shape2Qiymati}%,
     #96c9e9 100% 
 );`;
 
-  shape_2.style.background = shape2_style;
-}
-shapeColor();
+shape_1.setAttribute("style", `background: ${shape1_style}`);
+shape_2.setAttribute("style", `background: ${shape2_style}`);
