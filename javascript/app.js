5953f31a69d77dad5601ebf5fac79b61fa80194f
azizkhonteacher <azizkhonteacher@gmail.com>
2024-04-13 22:23:36 +0500

    json data err

--- javascript/app.js
+++ javascript/app.js
@@ -24,3 +24,52 @@ const shape2_style = `conic-gradient(
 
 shape_1.setAttribute("style", `background: ${shape1_style}`);
 shape_2.setAttribute("style", `background: ${shape2_style}`);
+
+//           team swiper
+const teamSwipetLeftBtn = document.getElementById("t-s-left-btn");
+const teamSwipetRightBtn = document.getElementById("t-s-right-btn");
+const swiperItems = document.querySelector(".swiper-items");
+
+swiperItems.addEventListener("wheel", (evt) => {
+  evt.preventDefault();
+  swiperItems.scrollLeft += evt.deltaY;
+  swiperItems.style.scrollBehavior = "auto";
+});
+
+teamSwipetRightBtn.addEventListener("click", () => {
+  swiperItems.style.scrollBehavior = "smooth";
+  swiperItems.scrollLeft += 380;
+});
+
+teamSwipetLeftBtn.addEventListener("click", () => {
+  swiperItems.style.scrollBehavior = "smooth";
+  swiperItems.scrollLeft -= 380;
+});
+
+// team swiper item json data
+const teamsWiperItem = document.querySelectorAll(".swiper-items .item");
+
+const teamGetTodos = (resurse) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.response);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject("Ma'lumot olinmadi ?!");
+      }
+    });
+    request.open("GET", resurse);
+    request.send();
+  });
+};
+
+
+teamGetTodos("./teamItems.json")
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
